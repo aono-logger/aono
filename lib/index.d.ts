eac35b35244bafcbecfd55d41a8b80c6249695ee
Maciej Chałapuk <m.chalapuk@quedex.net>
2018-05-15 14:39:36 +0200

    + exported Entry

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 export { Aono, default } from './Aono';
 export { Logger } from './Logger';
 export { Handler } from './Handler';
+export { Entry } from './Entry';
 export { TimeProvider } from './TimeProvider';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Aono, default } from './Aono';
 export { Logger } from './Logger';
 export { Handler } from './Handler';
+export { Entry } from './Entry';
 export { TimeProvider } from './TimeProvider';
 
